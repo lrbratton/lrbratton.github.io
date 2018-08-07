adecf133f7c69734d62e208663a22e5a5e14980c
lrbratton <l.bratton@hotmail.co.nz>
2018-08-07 21:53:30 +1200

    initial commit

---
+++ Go Jo/email_form.php
@@ -0,0 +1,22 @@
+<?php
+	
+
+	if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$email = $_POST['email'];
+	}
+	
+	$name = $_POST['name'];
+	$ph_no = $_POST['ph_no'];
+	$comment = $_POST['comment'];
+	
+
+	$email_subject = "Go Jo Webpage Email";
+	$email_body = "Hi,\n\n You have received a new email from $name.\n".
+					"Here is the message:\n $comment";
+
+	$to = "l.bratton@hotmail.co.nz";
+	$headers = "From: $email \r\n";
+	$headers = "Reply-To: $email \r\n";
+	mail($email,$email_subject,$email_body,$headers);
+	header('Location: contact_success.html')
+?>
